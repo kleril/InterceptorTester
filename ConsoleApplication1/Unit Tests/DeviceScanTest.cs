6de40a946a0b749e1d0ff6d744808dcc45ba6adf
lynneeai <lynneeai@hotmail.com>
2015-05-12 11:22:52 -0400

    added DeviceScanTest dynamic scan code

--- ConsoleApplication1/Unit Tests/DeviceScanTest.cs
+++ ConsoleApplication1/Unit Tests/DeviceScanTest.cs
@@ -16,15 +16,18 @@ namespace ConsoleApplication1
 	{
 		static Uri testServer = ServerUris.getLatest();
 
+		// simple scan code
+
 		// [TestMethod]
 		[Test]
-		// Valid Serial
-		public async Task ValidSerial()
+		// Valid Single Scan
+		public async Task ValidSingleScanSimple()
 		{
 
 			DeviceScanJSON testJson = new DeviceScanJSON ();
 			testJson.i = ValidSerialNumbers.getAll()[1];
 			testJson.d = "1289472198573";
+			testJson.b = null;
 			testJson.s = 4;
 			DeviceScan testDScan = new DeviceScan(testServer, testJson);
 
@@ -40,6 +43,189 @@ namespace ConsoleApplication1
 				Assert.AreEqual(nextTest.getExpectedResult(), nextTest.getActualResult());
 			}            
 		}
+
+
+		// dynamic scan code
+
+		// [TestMethod]
+		[Test]
+		// Valid Single Scan
+		public async Task ValidSingleScanDyn()
+		{
+			DeviceScanJSON testJson = new DeviceScanJSON ();
+			testJson.i = ValidSerialNumbers.getAll () [1];
+			testJson.d = "~20/90210|";
+			testJson.b = null;
+			testJson.s = 4;
+			DeviceScan testDScan = new DeviceScan (testServer, testJson);
+
+			Test scanTest = new Test (testDScan);
+
+			List<Test> tests = new List<Test> ();
+			tests.Add (scanTest);
+
+			await Program.buildTests (tests);
+
+			foreach (Test nextTest in Program.getTests()) 
+			{
+				Assert.AreEqual (nextTest.getExpectedResult (), nextTest.getActualResult ());
+			}
+		}
+
+		// [TestMethod]
+		[Test]
+		// Valid Call Home Scan
+		public async Task ValidCH()
+		{
+			DeviceScanJSON testJson = new DeviceScanJSON ();
+			testJson.i = ValidSerialNumbers.getAll () [1];
+			testJson.d = "~21/*CH*055577520928|";
+			testJson.b = null;
+			testJson.s = 4;
+			DeviceScan testDScan = new DeviceScan (testServer, testJson);
+
+			Test scanTest = new Test (testDScan);
+
+			List<Test> tests = new List<Test> ();
+			tests.Add (scanTest);
+
+			await Program.buildTests (tests);
+
+			foreach (Test nextTest in Program.getTests()) {
+				Assert.AreEqual (nextTest.getExpectedResult (), nextTest.getActualResult ());
+			}
+		}
+
+		// [TestMethod]
+		[Test]
+		// Invalid Scan Data
+		public async Task InvalidSingleScanDyn()
+		{
+			DeviceScanJSON testJson = new DeviceScanJSON ();
+			testJson.i = "BADSERIAL";
+			testJson.d = "~20|90210/";
+			testJson.b = null;
+			testJson.s = 4;
+			DeviceScan testDScan = new DeviceScan (testServer, testJson);
+
+			Test scanTest = new Test (testDScan);
+
+			List<Test> tests = new List<Test> ();
+			tests.Add (scanTest);
+
+			await Program.buildTests (tests);
+
+			foreach (Test nextTest in Program.getTests()) {
+				Assert.AreEqual (nextTest.getExpectedResult (), nextTest.getActualResult ());
+			}
+		}
+
+		// [TestMethod]
+		[Test]
+		// Bad Serial
+		public async Task InvalidSerialDyn()
+		{
+			DeviceScanJSON testJson = new DeviceScanJSON ();
+			testJson.i = "BADSERIAL";
+			testJson.d = "~20|noendingbar";
+			testJson.b = null;
+			testJson.s = 4;
+			DeviceScan testDScan = new DeviceScan (testServer, testJson);
+
+			Test scanTest = new Test (testDScan);
+
+			List<Test> tests = new List<Test> ();
+			tests.Add (scanTest);
+
+			await Program.buildTests (tests);
+
+			foreach (Test nextTest in Program.getTests()) {
+				Assert.AreEqual (nextTest.getExpectedResult (), nextTest.getActualResult ());
+			}
+		}
+
+		// [TestMethod]
+		[Test]
+		// No Serial
+		public async Task NoSerialDyn()
+		{
+			DeviceScanJSON testJson = new DeviceScanJSON ();
+			testJson.i = null;
+			testJson.d = "~20/90210|";
+			testJson.b = null;
+			testJson.s = 4;
+			DeviceScan testDScan = new DeviceScan (testServer, testJson);
+
+			Test scanTest = new Test (testDScan);
+
+			List<Test> tests = new List<Test> ();
+			tests.Add (scanTest);
+
+			await Program.buildTests (tests);
+
+			foreach (Test nextTest in Program.getTests()) {
+				Assert.AreEqual (nextTest.getExpectedResult (), nextTest.getActualResult ());
+			}
+		}
+
+		// [TestMethod]
+		[Test]
+		// List of Valid Scans
+		public async Task LOValidScansDyn()
+		{
+			DeviceScanJSON testJson = new DeviceScanJSON ();
+			testJson.i = null;
+			testJson.d = null;
+			string[] scanData = new string[4];
+			scanData[0] = "~20/0|";
+			scanData[1] = "~20/1|";
+			scanData[2] = "~20/2|";
+			scanData[3] = "~20/3|";
+			testJson.b = scanData;
+			testJson.s = 4;
+			DeviceScan testDScan = new DeviceScan (testServer, testJson);
+
+			Test scanTest = new Test (testDScan);
+
+			List<Test> tests = new List<Test> ();
+			tests.Add (scanTest);
+
+			await Program.buildTests (tests);
+
+			foreach (Test nextTest in Program.getTests()) {
+				Assert.AreEqual (nextTest.getExpectedResult (), nextTest.getActualResult ());
+			}
+		}
+
+		// [TestMethod]
+		[Test]
+		// Mixed of Valid/Invalid Scans
+		public async Task ValInvalScansDyn()
+		{
+			DeviceScanJSON testJson = new DeviceScanJSON ();
+			testJson.i = null;
+			testJson.d = null;
+			string[] scanData = new string[4];
+			scanData [0] = "~20/0|";
+			scanData [1] = "~20/noendingbar";
+			scanData [2] = "~20/2|";
+			scanData [3] = "~20/3|";
+			testJson.b = scanData;
+			testJson.s = 4;
+			DeviceScan testDScan = new DeviceScan (testServer, testJson);
+
+			Test scanTest = new Test (testDScan);
+		
+			List<Test> tests = new List<Test> ();
+			tests.Add (scanTest);
+
+			await Program.buildTests (tests);
+
+			foreach (Test nextTest in Program.getTests()) {
+				Assert.AreEqual (nextTest.getExpectedResult (), nextTest.getActualResult ());
+			}
+		}
+
 	}
 }
 

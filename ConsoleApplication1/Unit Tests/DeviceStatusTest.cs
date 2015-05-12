44a8400cd0806233eb78c5096274461945b6e693
kleril <kleril@users.noreply.github.com>
2015-05-12 15:44:53 -0400

    Got rid of unnecessary 'Using' calls

--- ConsoleApplication1/Unit Tests/DeviceStatusTest.cs
+++ ConsoleApplication1/Unit Tests/DeviceStatusTest.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 
 namespace ConsoleApplication1

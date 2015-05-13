96e61ee362f756c4d92ea3b775963baab5b276bf
kleril <kleril@users.noreply.github.com>
2015-05-13 15:12:33 -0400

    Gave jsons missing toStrings

--- ConsoleApplication1/JSON/DeviceSettingsJSON.cs
+++ ConsoleApplication1/JSON/DeviceSettingsJSON.cs
@@ -54,6 +54,12 @@ namespace ConsoleApplication1
             [DataMember]
             public string seqNum;
             // ReSharper restore InconsistentNaming
+
+            public override string ToString()
+            {
+                return string.Format("[DeviceStatus startURL: {1}, reportURL: {2}, scanURL: {3}, bkupURL: {4}, cmdURL: {5}, capture: {6}, captureMode: {7}, requestTimeoutValue: {8}, callHomeTimeoutMode: {9}, callHomeTimeoutData: {10}, dynCodeFormat: {11}, errorLog: {12}, wpaPSK: {13}, ssid: {14}, cmdChkInt: {15}, cTime: {16}, seqNum: {17}]",
+                    startURL, reportURL, scanURL, bkupURL, cmdURL, capture, captureMode, requestTimeoutValue, callHomeTimeoutMode, callHomeTimeoutData, dynCodeFormat, errorLog, wpaPSK, ssid, cmdChkInt, cTime, seqNum);
+            }
         }
 
 

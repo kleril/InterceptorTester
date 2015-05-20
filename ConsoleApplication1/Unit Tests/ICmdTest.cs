c65df720e7129a011542db6f607c6ce91a1e0999
lynneeai <lynneeai@hotmail.com>
2015-05-20 13:13:32 -0400

    ..

--- ConsoleApplication1/Unit Tests/ICmdTest.cs
+++ ConsoleApplication1/Unit Tests/ICmdTest.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using System.IO;
+using NetOffice.ExcelApi.Enums;
+using Excel = NetOffice.ExcelApi;
+
+//using Spire.Xls;
 
 namespace ConsoleApplication1
 {
@@ -27,7 +31,20 @@ namespace ConsoleApplication1
 
         static StreamWriter results;
 
+		/*
+		static Excel.Application xlsFile;
+
+		static Excel.Workbook workBook;
+
+		static Excel.Worksheet workSheet;
+
+		object misValue = System.Reflection.Missing.Value;
+		*/
+
         static string outputFile = "../../../logs/performanceTest" + DateTime.Now.ToFileTime() + ".txt";
+
+		//static string xlsPath = "../../../logs/xlsOutput" + DateTime.Now.ToFileTime () + ".xls";
+
 
         [TestFixtureSetUp]
         public void setup()
@@ -35,6 +52,24 @@ namespace ConsoleApplication1
             FileStream stream;
             stream = File.Create(outputFile);
             results = new StreamWriter(stream);
+
+			/*
+			try
+			{
+				results.WriteLine ("!!!!!!!!!!!!!!!!!!!!!!!!hey");
+				xlsFile = new Excel.Application();
+				workBook = xlsFile.Workbooks.Add(misValue);
+				workSheet = (Excel.Worksheet)workBook.ActiveSheet;
+				workSheet.Cells[1, 1].Value = "ICmd Performance Test";
+
+			}
+			catch (Exception e) 
+			{
+				results.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!" + e);
+			}
+			*/
+				
+
         }
 
         [TestFixtureTearDown]
@@ -49,6 +84,29 @@ namespace ConsoleApplication1
             results.WriteLine("Minimum Time: " + minTime);
             results.WriteLine("Maximum Time: " + maxTime);
             results.Close();
+
+			/*
+			try
+			{
+				workSheet.Cells[1, 1].Value = "Average Time";
+				workSheet.Cells[1, 2].Value = avgTime;
+				workSheet.Cells[2, 1].Value = "Maximum Time";
+				workSheet.Cells[2, 2].Value = maxTime;
+				workSheet.Cells[3, 1].Value = "Minimum Time";
+				workSheet.Cells[3, 2].Value = minTime;
+				workSheet.Cells[4, 1].Value = "95 Percentile";
+				workSheet.Cells[4, 2].Value = percentile;
+
+
+				workBook.SaveAs(xlsPath, misValue, misValue, misValue, false, false);
+				workBook.Close(true, misValue, misValue);
+				xlsFile.Quit();
+			}
+			catch (Exception e) 
+			{
+			}
+			*/
+
         }
 
         [Test, Repeat(maxReps)]
@@ -94,6 +152,7 @@ namespace ConsoleApplication1
 			}
 
             results.WriteLine("Test Time: " + time);
+
 
             foreach (Test nextTest in Program.getTests())
             {

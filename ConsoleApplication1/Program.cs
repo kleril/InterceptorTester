f86301d175957a590337a2902a5995bb7bf58ce5
lynneeai <lynneeai@hotmail.com>
2015-05-21 15:22:34 -0400

    ..

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -45,19 +45,6 @@ namespace ConsoleApplication1{
             tests.Add(validTest);
             */
 
-			try
-			{
-				XmlDocument doc = new XmlDocument();
-				XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
-				doc.AppendChild(docNode);
-				doc.Save("nunit-results.xml");
-				Console.WriteLine("!!!!!!!!!!!!yeah");
-			}
-			catch (Exception e) 
-			{
-				Console.WriteLine ("!!!!!!!!!!!" + e);
-			}
-
 
 			buildTests(tests);
         }
